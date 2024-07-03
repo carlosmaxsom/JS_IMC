3e1cf26e40bbb5096dea627f0731881c6434790d
Carlos M S Paixão <carlosmaxsom@gmail.com>
2024-07-03 12:54:30 -0300

    Ajuste com o cálculo do IMC (Dados da internet), ajuste das mensagens para retornar o IMC

--- imcfuncoes.js
+++ imcfuncoes.js
@@ -7,8 +7,27 @@ function calculaimc () {
     const resultado = document.getElementById('Resultado');
 
     if (nome !== '' && altura !== '' && peso !== '') {
-        // Funções aqui campos foram preenchidos
-        alert ('Ideia completa')
+        
+        const valorIMC = (peso / (altura ** 2)).toFixed(1);
+        
+        let classificacao = '';
+
+        if (valorIMC < 18.5){
+            classificacao = 'Abaixo do peso.';
+        }else if (valorIMC <= 25){
+            classificacao = 'com peso ideal. Muito bem!';
+        }else if (valorIMC < 30){
+            classificacao = 'levemente acima do peso.';
+        }else if (valorIMC < 35){
+            classificacao = 'com obesidade grau I';
+        }else if (valorIMC < 40){
+            classificacao = 'com obsesidade grau II';
+        }else {
+            classificacao = 'com obsidade grau III. Tome cuidado!';
+        }
+
+        resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao}.`
+
     } else {
         alert ('Preencha todos os campos')
     }
